118534d2f2f277770d2165ef88c605f3fa519de5
Cloudy32 <r.bondar29@gmail.com>
2026-06-18 11:13:14 +0300

    create restaurant repository and service

---
+++ app/repositories/restaurant_repo.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models.restaurant import Restaurant
+
+
+class RestaurantRepository:
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def get_by_id(self, restaurant_id: int) -> Restaurant | None:
+        stmt = select(Restaurant).where(Restaurant.id == restaurant_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def get_all(self, limit: int = 10) -> list[Restaurant]:
+        stmt = select(Restaurant).limit(limit).order_by(Restaurant.id)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def create(
+            self,
+            name: str,
+            city: str,
+            address: str,
+            description: str | None,
+            average_check: int | None,
+            rating: Decimal | None,
+            latitude: Decimal | None,
+            longitude: Decimal | None,
+    ) -> Restaurant:
+
+        restaurant = Restaurant(
+            name=name,
+            city=city,
+            address=address,
+            description=description,
+            average_check=average_check,
+            rating=rating,
+            latitude=latitude,
+            longitude=longitude
+        )
+
+        self.session.add(restaurant)
+        await self.session.commit()
+        await self.session.refresh(restaurant)
+        return restaurant
