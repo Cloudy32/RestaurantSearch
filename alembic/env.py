81331a61a3d451addbaf69d7f812c204dd90b313
Cloudy32 <r.bondar29@gmail.com>
2026-08-07 14:05:37 +0300

    Remove unused placeholder files

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from alembic import context
 
 from app.core.config import get_settings
 from app.db.base import Base
-from app.db.models.restaurant import Restaurant
+import app.db.models
 
 settings = get_settings()
 
